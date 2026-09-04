79a800e48ae81fd184c9c0ec92fcc7b830df910d
Sherry2023218 <125686289+Sherry2023218@users.noreply.github.com>
2026-09-04 13:04:02 +0800

    Update build.sh

--- targets/armsr/build.sh
+++ targets/armsr/build.sh
@@ -9,4 +9,8 @@ make image \
 
 make image PROFILE=generic PACKAGES="$PKGS" ROOTFS_TAR=./rootfs.tar.gz V=s
 
+ROOT_DIR="./build_dir/target-aarch64_generic_musl/root-armsr"
+tar --exclude=./dev -czf ./rootfs.tar.gz -C "${ROOT_DIR}" .
+
+echo "Generated rootfs.tar.gz:"
 ls -lh ./rootfs.tar.gz
